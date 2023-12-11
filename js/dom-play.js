885dd63d711fd5349e42d09807000e1b238fdc22
Daniella Echevestre <d.echevestre@gmail.com>
2023-12-11 13:40:10 -0800

    Added a toggle highlight function

---
+++ js/dom-play.js
@@ -0,0 +1,12 @@
+//alert("Hello from DOM Play!");
+
+//document.querySelector(".chandler").style.backgroundColor=("orange")
+
+function highlight(el){
+    if(el.style.backgroundColor== 'green'){
+        el.style.backgroundColor='white';
+    }else{
+        el.style.backgroundColor='green';
+    }
+    
+}
